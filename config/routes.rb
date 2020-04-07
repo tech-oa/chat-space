5841981162963d9aa2c963e903e446f89771cf0e
okiasu <62542016+okiasu@users.noreply.github.com>
2020-04-07 18:59:03 +0900

    view-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
+# ルーティングを追加
 Rails.application.routes.draw do
   devise_for :users
 
   root "messages#index"
   resources :users, only: [:edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
 end
