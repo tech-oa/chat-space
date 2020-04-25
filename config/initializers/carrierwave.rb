cf4a056a92e5662e7f65c161048d3b5cb0961b7e
okiasu <62542016+okiasu@users.noreply.github.com>
2020-04-25 11:25:30 +0900

    update-carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-2'
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'chat-space0219'
-  config.asset_host = 'https://s3-us-east-2.amazonaws.com/cha-space0219'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cha-space0219'
 end
